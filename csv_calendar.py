b2294d5ea3817c4ca503af189c605995e9b2a073
vomotom <vomotom@gmail.com>
2023-11-21 19:40:35 +0100

    debug
    debugged removes of wrong zeros

--- csv_calendar.py
+++ csv_calendar.py
@@ -22,8 +22,14 @@ def get_month_text(year, month_index):
     text += ",".join(days_of_week) + "\n" # days of a week
 
     for week in month:
-        row = ",".join(str(day) for day in week)
-        row = row.replace("0", "") # we want blank cells
+        week_row = ""
+        week_without_zeros = []
+        for day in week:
+            if day == 0:
+                week_without_zeros.append("") # we want blank cells
+            else:
+                week_without_zeros.append(str(day))
+        row = ",".join(week_without_zeros)
         text += row + "\n"
 
     text += "\n" # split months
